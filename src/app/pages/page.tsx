cf39a1d7995ada6508f5b5dd09b448d21c512503
codenexusplus <166818398+codenexusplus@users.noreply.github.com>
2025-01-23 21:16:18 +0500

    Update page.tsx

--- src/app/pages/page.tsx
+++ src/app/pages/page.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import Error404 from '../404Error/page'
-const Pages = () => {
+import React from 'react';
+
+const Error404 = () => {
   return (
     <div>
-    <Error404 />
+      <h1>404 - Page Not Found</h1>
+      <p>The page you are looking for does not exist.</p>
     </div>
-  )
-}
+  );
+};
 
-export default Pages
+export default Error404;
